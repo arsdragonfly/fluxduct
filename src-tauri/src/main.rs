a8865d06c328e9e8ea4013414f0d624a0e90541d
Zheyu Shen <arsdragonfly@gmail.com>
2022-05-21 21:39:45 +0800

    emit messages to the frontend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,25 +3,36 @@
   windows_subsystem = "windows"
 )]
 
-use pipewire::{MainLoop, Context};
-
-fn main() {
-  let (pw_sender, pw_receiver) = pipewire::channel::channel::<()>();
-  let pw_thread = std::thread::spawn(move || pw_thread_main(pw_receiver));
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
-  pw_thread.join().expect("Pipewire thread panicked");
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+  message: String,
 }
 
-fn pw_thread_main(pw_receiver: pipewire::channel::Receiver<()>) {
+use pipewire::{MainLoop, Context};
+use tauri::Manager;
+fn main() {
+  let (tauri_to_pw_sender, tauri_to_pw_receiver) = pipewire::channel::channel::<()>();
+  tauri::Builder::default()
+    .setup(|app| {
+      let handle = app.handle();
+      let pw_thread = std::thread::spawn(move || pw_thread_main(handle, tauri_to_pw_receiver));
+      Ok(())
+      })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
+
+fn pw_thread_main(app: tauri::AppHandle, tauri_to_pw_receiver: pipewire::channel::Receiver<()>) {
   let mainloop = MainLoop::new().expect("Failed to create mainloop");
   let context = Context::new(&mainloop).expect("Failed to create context");
   let core = context.connect(None).expect("Failed to connect to remote");
   let registry = core.get_registry().expect("Failed to get registry");
   let _listener = registry
     .add_listener_local()
-    .global(|global| println!("New global : {:?}", global))
+    .global(move |global| {
+      println!("New global : {:?}", global);
+      app.emit_all("pipewire_global", Payload { message: format!("{:?}", global).into() }).unwrap();
+    })
     .register();
   mainloop.run();
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { emit, listen } from '@tauri-apps/api/event'
+import util from 'util';
 
 function App() {
+  const [str, setStr] = useState("");
+  useEffect(() => {
+    const unlisten = listen('pipewire_global', event => {
+      setStr(util.inspect(event.payload));
+    })
+    return () => {
+      unlisten.then(f => f())
+    }
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>{str}</p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
