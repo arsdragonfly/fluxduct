5f12ebe82f6f88bfad8cf358c3336719fe01c51c
Zheyu Shen <arsdragonfly@gmail.com>
2025-09-12 02:53:16 -0700

    tune eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,24 @@
 // @ts-check
 
-import globals from 'globals';
-import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 
 export default defineConfig(
-    eslint.configs.recommended,
-    tseslint.configs.recommended,
-    {
-        languageOptions: {
-            ecmaVersion: 2024,
-            globals: globals.browser,
-        }
-    }
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  reactHooks.configs["recommended-latest"],
+  {
+    plugins: {
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  }
 );
